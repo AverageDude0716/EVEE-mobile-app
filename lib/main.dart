6f4f68757cbfd358197ad171d10e69a9aaab5dc5
Main <joshandre071605@gmail.com>
2023-05-18 17:36:00 +0800

    Added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,17 @@
 import 'package:evee/landing_page.dart';
 import 'package:evee/login.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() 
-{
+async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MainApp());
 }
 
